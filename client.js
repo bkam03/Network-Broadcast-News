15c88b610699ef09941f1f4fe84528f2ba20a37f
BK <bkam03@yahoo.com>
2017-06-30 15:51:58 -1000

    added instructions

---
+++ client.js
@@ -0,0 +1,7 @@
+/*
+Create a new net.Socket and connect to your running socket server.
+
+Once connected, pipe your terminal's standard input stream to write to your connected socket.
+
+Whenever the connected socket (client) emits a 'data' event, then data is being broadcasted from the server, pipe that data out to your terminal's standard output stream.
+*/
