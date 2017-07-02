10a2cc2502d2df85a391a2a54bc8c037a1abc0a8
BK <bkam03@yahoo.com>
2017-07-01 14:51:53 -1000

    display text coming in by console.log

--- client.js
+++ client.js
@@ -13,13 +13,18 @@ var socket = net.createConnection( 6969, '0.0.0.0', function(){
   console.log( 'there is a client connection' );
 
   //encoding
-  //socket.setEncoding( 'utf8' );
+  socket.setEncoding( 'utf8' );
 
-  process.stdin.on( 'data', function( chunk ){
-    console.log( 'stdin: ' + chunk );
-    socket.write( chunk );
+    process.stdin.on( 'data', function( chunk ){
+      //console.log( 'stdin: ' + chunk );
+      socket.write( chunk );
     //console.log( socket );
-  });
+    });
+
+    socket.on( 'data', function( chunk ){
+      console.log( chunk );
+
+    } );
 
 /*  socket.on( 'data', function( chunk ){
     process.stdout.write( chunk );
