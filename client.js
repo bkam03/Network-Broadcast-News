067782183e1a4d47f341bd2c44ad49cc3a8a0d57
BK <bkam03@yahoo.com>
2017-07-01 14:32:37 -1000

    when you type somethig the string is captured and written to socket supposedly

--- client.js
+++ client.js
@@ -5,3 +5,26 @@ Once connected, pipe your terminal's standard input stream to write to your conn
 
 Whenever the connected socket (client) emits a 'data' event, then data is being broadcasted from the server, pipe that data out to your terminal's standard output stream.
 */
+
+const net = require( 'net' );
+
+
+var socket = net.createConnection( 6969, '0.0.0.0', function(){
+  console.log( 'there is a client connection' );
+
+  //encoding
+  //socket.setEncoding( 'utf8' );
+
+  process.stdin.on( 'data', function( chunk ){
+    console.log( 'stdin: ' + chunk );
+    socket.write( chunk );
+    //console.log( socket );
+  });
+
+/*  socket.on( 'data', function( chunk ){
+    process.stdout.write( chunk );
+    console.log( 'socket stdout' );
+  });*/
+} );
+
+//this socket is connected
